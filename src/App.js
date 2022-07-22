c686d250a2e9a845fd47180d6f92d025235cb661
suhan ahmed <98055493+suhan647@users.noreply.github.com>
2022-07-22 11:17:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Card from './components/Card';
+
+
+export default function App() {
+
+  return (
+    <>
+   
+  <Navbar/> 
+  {/* <Hero/> */}
+<Card/>
+    </>
+  )
+}
---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Hero.css"
+
+
+
+function Hero() {
+  return (
+    <section className='hero'>
+   <img  src='https://i7x7p5b7.stackpathcdn.com/codrops/wp-content/uploads/2015/06/CardExpansion.jpg' alt='images' />
+    <h1>Online Experiences</h1>
+    <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</p>
+
+    </section>
+  )
+}
+
+export default Hero
